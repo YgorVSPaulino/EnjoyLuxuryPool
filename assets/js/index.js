be7ceceaa5f5b3ccd225789bbedd50b0f975f191
YgorVSPaulino <ygor.paulino.1999@gmail.com>
2024-12-10 12:19:36 -0300

    atualizando projeto

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,3 +9,15 @@ botaoMenu.addEventListener('click', () => {
     if (opcoesMenu.classList.contains('mostrar')) opcoesMenu.classList.remove('mostrar')
     else opcoesMenu.classList.add('mostrar')
 })
+
+document.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
+    const section2 = document.querySelector("#section2");
+    const section2Top = section2.getBoundingClientRect().top;
+    
+    if (section2Top <= 50) { // Quando a seção 2 estiver quase visível
+      nav.classList.add("nav-glass");
+    } else {
+      nav.classList.remove("nav-glass");
+    }
+  });
